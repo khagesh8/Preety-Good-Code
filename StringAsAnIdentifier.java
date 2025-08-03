102d1a94c4ac426ac698b13164caff3cdfd7c295
KhageshUmbarkar <khageshumbarkar9@gmail.com>
2025-08-03 11:11:53 +0530

    String as an Identifier
    Yes, we can use "String" as a variable name or identifier, and also we can create String object in the same file. "String" is a valid identifier as it starts with a non-numeric character, doesn't have any special characters, no whitespaces.

---
+++ StringAsAnIdentifier.java
@@ -0,0 +1,8 @@
+class StringAsAnIdentifier{
+	public static void main(String[] args) {
+		int String = 123;		// "String" is a valid Identifier, as it follows all rules of identifiers declaration
+		System.out.println(String);
+		String str = new String("hello");	// here it'll create String Object 
+		System.out.println(str);
+	}
+}
